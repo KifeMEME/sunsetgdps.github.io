cb3184ede714211b8a8ece3f2c09a21018f1929d
sunsetgdps <muskusgerbarij@gmail.com>
2024-07-04 20:51:26 +0300

    Update loadUL.js

--- scripts/loadUL.js
+++ scripts/loadUL.js
@@ -3,6 +3,8 @@ const add = new DLCore();
 $.get('./demonlist/users.txt?' + add.getRandomInt(100000, 999999), (data) => {
     const lines = data.split('\n');
     for(let i = 0; i < lines.length; i++) {
+        if(!lines[i]) continue;
+
         const params = lines[i].split(';');
         add.addUsers(params[0]);
     }
